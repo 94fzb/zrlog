828ebe699eb17c09ddf43ff89de52d8f7946219b
xiaochun <xchun90@163.com>
2016-12-11 17:13:07 +0800

    optimize upgrade process

--- src/main/java/com/fzb/blog/web/plugin/UpdateVersionPlugin.java
+++ src/main/java/com/fzb/blog/web/plugin/UpdateVersionPlugin.java
@@ -15,6 +15,7 @@ import org.apache.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class UpdateVersionPlugin implements IPlugin {
@@ -90,49 +91,74 @@ public class UpdateVersionPlugin implements IPlugin {
         sb.append("<p>").append(str).append("</p>");
     }
 
-    public void startUpgrade(File file) {
+    public void startUpgrade(final File file) {
         updating = true;
-        try {
-            updateProcessMsg("开始更新");
-            String warName;
-            String contextPath = JFinal.me().getServletContext().getContextPath();
-            if (contextPath.equals("/") || contextPath.equals("")) {
-                warName = "/ROOT.war";
-            } else {
-                warName = contextPath + ".war";
+        final UpdateVersionPlugin plugin = this;
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    updateProcessMsg("开始更新");
+                    String warName;
+                    String contextPath = JFinal.me().getServletContext().getContextPath();
+                    String folderName;
+                    if (contextPath.equals("/") || contextPath.equals("")) {
+                        warName = "/ROOT.war";
+                        folderName = "ROOT/";
+                    } else {
+                        warName = contextPath + ".war";
+                        folderName = contextPath;
+                    }
+                    String filePath = System.getProperty("java.io.tmpdir") + "/" + UUID.randomUUID().toString();
+                    String backupFolder = new File(PathKit.getWebRootPath()).getParentFile().getParentFile() + "/backup/" + new SimpleDateFormat("yyyy-MM-dd_HH:mm").format(new Date()) + "/";
+                    updateProcessMsg("历史版本备份在 " + backupFolder);
+                    new File(backupFolder).mkdir();
+                    IOUtil.moveOrCopy(new File(PathKit.getWebRootPath()).getParent() + "/" + folderName, backupFolder, false);
+                    IOUtil.moveOrCopy(new File(PathKit.getWebRootPath()).getParent() + "/" + warName, backupFolder, false);
+                    updateProcessMsg("生成临时文件");
+                    File tempFilePath = new File(filePath);
+                    tempFilePath.mkdir();
+                    IOUtil.moveOrCopy(file.toString(), tempFilePath.getParentFile().toString(), false);
+                    String tempFile = tempFilePath.getParentFile() + "/" + file.getName();
+                    updateProcessMsg("解压文件");
+                    ZipUtil.unZip(tempFile, filePath + "/");
+                    updateProcessMsg("解压完成");
+                    List<File> fileList = new ArrayList<File>();
+                    fileList.add(new File(filePath));
+                    File tempWarFile = new File(tempFilePath.getParent() + "/" + warName);
+                    LOGGER.info(tempWarFile);
+                    IOUtil.moveOrCopy(PathKit.getWebRootPath() + "/WEB-INF/db.properties", filePath + "/WEB-INF/", false);
+                    IOUtil.moveOrCopy(PathKit.getWebRootPath() + "/WEB-INF/install.lock", filePath + "/WEB-INF/", false);
+                    File templatePath = new File(PathKit.getWebRootPath() + Constants.TEMPLATE_BASE_PATH);
+                    File[] templates = templatePath.listFiles();
+                    if (templates != null && templates.length > 0) {
+                        for (File template : templates) {
+                            if (template.isDirectory() && template.toString().substring(PathKit.getWebRootPath().length()).equals(Constants.DEFAULT_TEMPLATE_PATH)) {
+                                LOGGER.info("skip default template folder");
+                                continue;
+                            }
+                            IOUtil.moveOrCopy(template.toString(), filePath + "/" + Constants.TEMPLATE_BASE_PATH, false);
+                        }
+                    }
+                    if (new File(PathKit.getWebRootPath() + "/attached").exists()) {
+                        IOUtil.moveOrCopy(PathKit.getWebRootPath() + "/attached", filePath, false);
+                    }
+                    ZipUtil.inZip(fileList, filePath + "/", tempWarFile.toString());
+                    File finalFile = new File(new File(PathKit.getWebRootPath()).getParentFile() + warName);
+                    updateProcessMsg("覆盖安装包文件");
+                    finalFile.delete();
+                    LOGGER.info("finalFile " + finalFile);
+                    IOUtil.moveOrCopy(tempWarFile.toString(), finalFile.getParentFile().toString(), false);
+                    updateProcessMsg("升级完成");
+                    process = 100;
+                    plugin.stop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    LOGGER.error(e);
+                    updateProcessMsg("升级失败\n" + e.getMessage());
+                }
             }
-            String filePath = System.getProperty("java.io.tmpdir") + "/" + UUID.randomUUID().toString();
-            File tempFilePath = new File(filePath);
-            tempFilePath.mkdir();
-            updateProcessMsg("生成临时文件");
-            IOUtil.moveOrCopy(file.toString(), tempFilePath.getParentFile().toString(), false);
-            String tempFile = tempFilePath.getParentFile() + "/" + file.getName();
-            updateProcessMsg("解压文件");
-            ZipUtil.unZip(tempFile, filePath + "/");
-            updateProcessMsg("解压完成");
-            List<File> fileList = new ArrayList<File>();
-            fileList.add(new File(filePath));
-            File tempWarFile = new File(tempFilePath.getParent() + "/" + warName);
-            System.out.println(tempWarFile);
-            IOUtil.moveOrCopy(PathKit.getWebRootPath() + "/WEB-INF/db.properties", filePath + "/WEB-INF/", false);
-            IOUtil.moveOrCopy(PathKit.getWebRootPath() + "/WEB-INF/install.lock", filePath + "/WEB-INF/", false);
-            if (new File(PathKit.getWebRootPath() + "/attached").exists()) {
-                IOUtil.moveOrCopy(PathKit.getWebRootPath() + "/attached", filePath, false);
-            }
-            ZipUtil.inZip(fileList, filePath + "/", tempWarFile.toString());
-            File finalFile = new File(new File(PathKit.getWebRootPath()).getParentFile() + warName);
-            updateProcessMsg("覆盖安装包文件");
-            finalFile.delete();
-            LOGGER.info("finalFile " + finalFile);
-            IOUtil.moveOrCopy(tempWarFile.toString(), finalFile.getParentFile().toString(), false);
-            updateProcessMsg("升级完成");
-            process = 100;
-            stop();
-        } catch (Exception e) {
-            e.printStackTrace();
-            LOGGER.error(e);
-            updateProcessMsg("升级失败\n" + e.getMessage());
-        }
+        }.start();
     }
 
     public boolean isUpdating() {
--- src/main/webapp/include/templates/default/comment.jsp
+++ src/main/webapp/include/templates/default/comment.jsp
@@ -4,28 +4,28 @@
 <c:choose>
 <c:when test="${log.canComment}">
 <c:choose>
-    <c:when test="${init.webSite.user_comment_pluginStatus}">
+    <c:when test="${init.webSite.duoshuo_status eq 'on'}">
          <jsp:include page="../../core/duoshuo_comment.jsp"></jsp:include>
     </c:when>
     <c:otherwise>
          <c:if test="${not empty log.comments}">
-           <h2>评论列表</h2></c:if>
+           <h2>${_res.comments}</h2></c:if>
          <c:forEach items="${log.comments}" var="comment">
            <ul class="comments">
-            <li>	<p>${comment.userComment}</p>
-         <p class="small"><a rel="nofollow" href="${comment.userHome }">${comment.userName }</a> 在 ${comment.commTime }</p>
+            <li><p>${comment.userComment}</p>
+         <p class="small"><a rel="nofollow" >${comment.userName }</a> ${_res.on} ${comment.commTime }</p>
             </li></ul>
          </c:forEach>
          <div id="cwrapper">
          <form action="${rurl }post/addComment" method="post" id="txpCommentInputForm">
          <div class="comments-wrapper">
          <div class="reply">
-           <h2 id="comment">给我留言 <span class="en">Say something...</span></h2>
+           <h2 id="comment">${_res.comment}</h2>
              <p class="comment-write"><label class="hidden" for="message">Message</label><textarea class="txpCommentInputMessage" rows="15" cols="45" name="userComment" id="message"></textarea></p>
              <div class="input-group">
-               <p><label for="userName">姓名</label><input type="text" id="name" class="comment_name_input" size="25" name="userName" value=""></p>
-               <p><label for="userMail">邮箱</label><input type="text" id="email" class="comment_email_input" size="25" name="userMail" value=""></p>
-               <p><label for="webHome">网站</label><input type="text" id="web" class="comment_web_input" size="25" name="webHome" value=""></p>
+               <p><label for="userName">${_res.name}</label><input type="text" id="name" class="comment_name_input" size="25" name="userName" value=""></p>
+               <p><label for="userMail">${_res.email}</label><input type="text" id="email" class="comment_email_input" size="25" name="userMail" value=""></p>
+               <p><label for="webHome">${_res.website}</label><input type="text" id="web" class="comment_web_input" size="25" name="webHome" value=""></p>
              </div>
              <div class="button-set">
                 <span class="submit"><input type="submit"  id="txpCommentSubmit" class="button" name="submit" value="Submit"></span>
