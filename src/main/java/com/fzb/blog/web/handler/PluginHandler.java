f9a84ff1b1d2b2c71decb413d593827947edb862
xiaochun <xchun90@163.com>
2016-12-31 10:50:19 +0800

    add comment for PluginHandler

--- src/main/java/com/fzb/blog/web/handler/PluginHandler.java
+++ src/main/java/com/fzb/blog/web/handler/PluginHandler.java
@@ -16,6 +16,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 这个类负责了对所有的插件路由的代理中转给插件服务（及plugin-core.jar这个进程），使用的是Handler而非Interception也是由于Interception
+ * 不处理静态请求。
+ * 目前插件服务拦截了
+ * 1./admin/plugins/* （进行权限检查）
+ * 2./plugin/* /p/* （是plugin的短路由，代码逻辑上是不区分的，不检查权限）
+ *
+ * 如果想了解更多关于插件的实现可以浏览这篇文章 http://blog.zrlog.com/post/zrlog-plugin-dev
+ */
 public class PluginHandler extends Handler {
 
     private static final Logger LOGGER = Logger.getLogger(PluginHandler.class);
@@ -43,6 +52,14 @@ public class PluginHandler extends Handler {
         }
     }
 
+    /**
+     * 检查是否登陆，未登陆的请求直接放回403的错误页面
+     * @param target
+     * @param request
+     * @param response
+     * @throws IOException
+     * @throws InstantiationException
+     */
     private void adminPermission(String target, HttpServletRequest request, HttpServletResponse response) throws IOException, InstantiationException {
         User user = (User) request.getSession().getAttribute("user");
         if (user != null) {
@@ -54,33 +71,53 @@ public class PluginHandler extends Handler {
         }
     }
 
+    /**
+     * 不检查是否登陆，错误请求直接直接转化为403
+     * @param target
+     * @param request
+     * @param response
+     * @throws IOException
+     * @throws InstantiationException
+     */
     private void visitorPermission(String target, HttpServletRequest request, HttpServletResponse response) throws IOException, InstantiationException {
         if (!accessPlugin(target.replace("/plugin", "").replace("/p", ""), request, response)) {
             response.sendError(403);
         }
     }
 
+    /**
+     * 代理中转HTTP请求，目前仅支持，GET，POST 请求方式的中转。
+     * @param uri
+     * @param request
+     * @param response
+     * @return true 表示请求正常执行，false 代表遇到了一些问题
+     * @throws IOException
+     * @throws InstantiationException
+     */
     private boolean accessPlugin(String uri, HttpServletRequest request, HttpServletResponse response) throws IOException, InstantiationException {
         String pluginServerHttp = ZrlogUtil.getPluginServer();
         CloseableHttpResponse httpResponse;
         CloseResponseHandle data = new CloseResponseHandle();
         Map<String, String[]> paramMap = request.getParameterMap();
+        //GET请求不关心request.getInputStream() 的数据
         if ("GET".equals(request.getMethod())) {
             httpResponse = HttpUtil.sendGetRequest(pluginServerHttp + uri, paramMap, data, ZrlogUtil.genHeaderMapByRequest(request)).getT();
         } else {
+            //如果是表单数据提交不关心请求头，反之将所有请求头都发到插件服务
             if (request.getHeader("Content-Type").contains("application/x-www-form-urlencoded")) {
                 httpResponse = HttpUtil.sendPostRequest(pluginServerHttp + uri, paramMap, data, ZrlogUtil.genHeaderMapByRequest(request)).getT();
             } else {
                 httpResponse = HttpUtil.sendPostRequest(pluginServerHttp + uri, IOUtil.getByteByInputStream(request.getInputStream()), data, ZrlogUtil.genHeaderMapByRequest(request)).getT();
             }
         }
+        //添加插件服务的HTTP响应头到调用者响应头里面
         if (httpResponse != null) {
             Map<String, String> headerMap = new HashMap<String, String>();
             Header[] headers = httpResponse.getAllHeaders();
             for (Header header : headers) {
                 headerMap.put(header.getName(), header.getValue());
             }
-
+            //防止多次被Transfer-Encoding
             headerMap.remove("Transfer-Encoding");
             LOGGER.info("--------------------------------- response");
             for (Map.Entry<String, String> t : headerMap.entrySet()) {
@@ -88,7 +125,7 @@ public class PluginHandler extends Handler {
                 LOGGER.info("key " + t.getKey() + " value-> " + t.getValue());
             }
         }
-
+        //将插件服务的HTTP的body返回给调用者
         if (data.getT() != null && data.getT().getEntity() != null) {
             request.getSession();
             byte[] bytes = IOUtil.getByteByInputStream(data.getT().getEntity().getContent());
