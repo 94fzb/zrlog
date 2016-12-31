4306d7cd29c8eb16c3c036d0a4131eec784b01b1
xiaochun <xchun90@163.com>
2016-12-31 12:53:00 +0800

    add comment to WebTools

--- src/main/java/com/fzb/blog/web/util/WebTools.java
+++ src/main/java/com/fzb/blog/web/util/WebTools.java
@@ -2,8 +2,16 @@ package com.fzb.blog.web.util;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 存放与Web相关的工具代码
+ */
 public class WebTools {
 
+    /**
+     * 处理由于浏览器使用透明代理，或者是WebServer运行在诸如 nginx/apache 这类 HttpServer后面的情况，通过获取请求头真实IP地址
+     * @param request
+     * @return
+     */
     public static String getRealIp(HttpServletRequest request) {
         String ip = request.getHeader("X-forwarded-for");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
