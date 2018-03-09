f57883434473930a6a11962066d99c0bf28decd1
xiaochun <xchun90@163.com>
2018-03-09 22:05:27 +0800

    fix #8

--- common/src/test/java/com/zrlog/util/test/OutlineTest.java
+++ common/src/test/java/com/zrlog/util/test/OutlineTest.java
@@ -1,9 +1,7 @@
 package com.zrlog.util.test;
 
-import com.zrlog.common.vo.Outline;
+import com.zrlog.common.vo.OutlineVO;
 import com.zrlog.util.OutlineUtil;
-
-import java.util.List;
 
 public class OutlineTest {
 
@@ -11,7 +9,7 @@ public class OutlineTest {
     public static void main(String[] args) {
         //String htmlStr = "<h1>H1</h1><h2>H2</h2><h3>h3</h3><h2>H2-2</h2><h3>h3-2</h3><h3>h3-3</h3><h4>h4</h4><h2>h2</h2><h1>h1</h1>";
         String htmlStr = "<h3>h3</h3><h2>h2</h2><h2>h2</h2><h4>h4</h4>";
-        List<Outline> outlineList = OutlineUtil.extractOutline(htmlStr);
+        OutlineVO outlineList = OutlineUtil.extractOutline(htmlStr);
         String baseHtml = OutlineUtil.buildTocHtml(outlineList, "");
         System.out.println("baseHtml = " + baseHtml);
     }
