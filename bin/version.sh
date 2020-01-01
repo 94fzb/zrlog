768d7a6d4c33146cd42c9274999aea3f91e9fe78
xiaochun <xchun90@163.com>
2020-01-01 12:47:38 +0800

    [shell-release]release version 2.1.7

--- bin/version.sh
+++ bin/version.sh
@@ -1,7 +1,15 @@
 baseVersion=2.1
 releaseVersion=${baseVersion}.${1}
-nextVersion=${baseVersion}.$((${1}+1))
+nextVersion=${baseVersion}.$((${1}+1))-SNAPSHOT
 ./mvnw versions:set -DnewVersion=${releaseVersion}
 git add -A
 git commit -m '[shell-release]release version '${releaseVersion}
+git push
+git checkout release
+git reset --hard master
+git push origin release -f
+git checkout master
+./mvnw versions:set -DnewVersion=${nextVersion}
+git add -A
+git commit -m '[shell-release]next version '${nextVersion}
 git push
