74a9fb89c3d8ae832b5be9c36976ef69ddc75838
xiaochun <xchun90@163.com>
2020-11-07 17:10:08 +0800

    Optimize ammin reosurce data load

--- admin-frontend/src/components/base-resource-component.js
+++ admin-frontend/src/components/base-resource-component.js
@@ -32,16 +32,27 @@ export class BaseResourceComponent extends React.Component {
         return 200;
     }
 
+    handleRes(data){
+        data.data.copyrightTips = data.data.copyright + ' ZrLog';
+        this.setState({
+            res: data.data,
+            resLoading: false
+        });
+        this.fetchResSuccess(data.data);
+        document.title = [this.getSecondTitle(), data.data['admin.management'], this.state.res.websiteTitle].filter(Boolean).join(" | ");
+    }
+
     fetchRes() {
-        axios.get('/api/public/resource').then(({data}) => {
-            data.data.copyrightTips = data.data.copyright + ' ZrLog';
-            this.setState({
-                res: data.data,
-                resLoading: false
-            });
-            this.fetchResSuccess(data.data);
-            document.title = [this.getSecondTitle(), data.data['admin.management'], this.state.res.websiteTitle].filter(Boolean).join(" | ");
-        })
+        const resourceKey = "commonRes";
+        const resourceData = window.sessionStorage.getItem(resourceKey);
+        if(resourceData === null) {
+            axios.get('/api/public/resource').then(({data}) => {
+                this.handleRes(data);
+                window.sessionStorage.setItem(resourceKey, JSON.stringify(data));
+            })
+        }else{
+            this.handleRes(JSON.parse(resourceData));
+        }
     }
 
     getSecondTitle() {
