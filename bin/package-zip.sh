d27b8fb258d3b95e95d09e29f90f77ee6164d3fb
xiaochun <xchun90@163.com>
2021-10-17 16:53:24 +0800

    Optimize package final name

--- bin/package-zip.sh
+++ bin/package-zip.sh
@@ -3,6 +3,7 @@ rm -rf web/src/main/lib
 rm -rf web/src/main/zrlog.*
 version=$(printf 'VER\t${project.version}' | ./mvnw help:evaluate | grep '^VER' | cut -f2)
 ./mvnw clean package
+mv web/src/main/zrlog-starter.jar target/zrlog-starter.jar
 cd target && mkdir webapp && cp zrlog-"${version}".war webapp/ROOT.war && cd webapp && unzip ROOT.war
 rm -rf WEB-INF/classes && rm WEB-INF/db.properties && rm WEB-INF/install.lock  && rm -rf WEB-INF/lib && rm -rf ROOT.war
 cd ..
