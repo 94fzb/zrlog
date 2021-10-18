8fa96d92d5287a65f69119e952738c1e50727ca9
xiaochun <xchun90@163.com>
2021-10-19 07:46:18 +0800

    Optimize start script

--- bin/mvn-tomcat-run.sh
+++ bin/mvn-tomcat-run.sh
@@ -1,4 +1,1 @@
-rm -rf web/src/main/lib
-rm -rf web/src/main/zrlog.*
-./mvnw clean package -Dmaven.war.skip -DfinalName=zrlog
-cd web/src/main/ && sh bin/run.sh
+./mvnw clean package -Dmaven.war.skip -DfinalName=zrlog && cd web/src/main/ && sh bin/run.sh
