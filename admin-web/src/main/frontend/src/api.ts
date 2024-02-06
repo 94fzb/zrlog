4789dc45dfec6937cdefe0bb5cd921d83576be6f
xiaochun <xchun90@163.com>
2024-02-06 11:26:19 +0800

    sff

--- admin-web/src/main/frontend/src/api.ts
+++ admin-web/src/main/frontend/src/api.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 import {ssData} from "./index";
 
-let ssPageData:undefined;
+let ssRendered :boolean = false;
 
 export const getCsrData = async (uri: string) => {
-    // @ts-ignore
-    if(ssPageData === undefined && ssData && ssData.pageData){
-        ssPageData = ssData.pageData;
+    // 仅客户端渲染时，第一次渲染时生效
+    if(!ssRendered && ssData && ssData.pageData){
+        ssRendered = true;
         return ssData.pageData;
     }
     const {data} = await axios.get("/api/admin" + uri);
