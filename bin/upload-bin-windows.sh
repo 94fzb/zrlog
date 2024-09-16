597ffd1299f153c090d058d8c094b6a699f00648
xiaochun <90xchun@gmail.com>
2024-09-16 09:30:03 +0800

    Update host info

--- bin/upload-bin-windows.sh
+++ bin/upload-bin-windows.sh
@@ -2,4 +2,4 @@ bash -e bin/download-upload-depenecy.sh || true
 #upload
 originalPath="${4}/${5}"
 echo ${originalPath}
-pscp -hostkey "ssh-ed25519 255 SHA256:npOpUqkVgzEG/WTKAJ1EoSc+2s41jxDgDk5rfpTOVN8" -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
+pscp -hostkey "ssh-ed25519 255 SHA256:yigNlkR2q11I5j72PBWqW1pWdZnxM3p98vIbuZC8zDY" -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
