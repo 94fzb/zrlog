d21c1cd61cefcef7d0d28ed793f9eb2b40ed5a52
xiaochun <90xchun@gmail.com>
2025-01-20 10:08:20 +0800

    Update ssh key error

--- bin/upload-bin-windows.sh
+++ bin/upload-bin-windows.sh
@@ -2,4 +2,4 @@ bash -e bin/download-upload-depenecy.sh || true
 #upload
 originalPath="${4}/${5}"
 echo ${originalPath}
-pscp -hostkey "ssh-ed25519 255 SHA256:giv70Ft9rK1W8MuuvVFa+SyuDj0TCrNMt3Na6mQLLGE" -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
+pscp -hostkey "ssh-ed25519 255 SHA256:FpsWLo+PNYccPlNgcfZKfOVAGIbawyEQ/xXzAmla1AY" -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
