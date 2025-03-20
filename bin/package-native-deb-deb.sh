d4727de8ece1cd8ed2dec31653e05a5b27018b83
xiaochun <90xchun@gmail.com>
2025-03-20 23:17:43 +0800

    Add build id

--- bin/package-native-deb-deb.sh
+++ bin/package-native-deb-deb.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sh bin/package-native.sh
 version=$(printf 'VER\t${project.version}' | ./mvnw help:evaluate | grep '^VER' | cut -f2)
-./mvnw -Pjdeb package
+buildId=$(git rev-parse --short HEAD)
+./mvnw -Pjdeb package -DgitCommitId=${buildId}
 mv package-web/target/*.deb zrlog-"${version}-$(uname -s)-$(dpkg --print-architecture)".deb
