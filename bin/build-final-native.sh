38f1af5902192fea7daeddb6e97f54d89be528a9
xiaochun <90xchun@gmail.com>
2025-03-26 00:48:45 +0800

    Fix date info error

--- bin/build-final-native.sh
+++ bin/build-final-native.sh
@@ -6,7 +6,7 @@ PWD=`pwd`
 mirrorWebSite=https://dl.zrlog.com/
 runMode=${1}
 runModeDesc=${2}
-Date=$(date +"%Y-%m-%d %H:%M:%S%z" | sed "s/\([+-]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1\2:\3/")
+Date=$(git log -1 --format=%cd --date=format:'%Y-%m-%d %H:%M:%S%z' | sed "s/\([+-]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1\2:\3/")
 buildId=$(git rev-parse --short HEAD)
 mkdir -p data/src/main/resources
 packageExt="zip"
--- bin/build-final-package.sh
+++ bin/build-final-package.sh
@@ -6,7 +6,7 @@ PWD=`pwd`
 mirrorWebSite=https://dl.zrlog.com/
 runMode=${1}
 runModeDesc=${2}
-Date=$(date --rfc-3339="seconds")
+Date=$(git log -1 --format=%cd --date=format:'%Y-%m-%d %H:%M:%S%z' | sed "s/\([+-]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1\2:\3/")
 buildId=$(git rev-parse --short HEAD)
 mkdir -p data/src/main/resources
 echo -e "version=${version}\nrunMode=${runMode}\nbuildId=${buildId}\nbuildTime=${Date}\nmirrorWebSite=${mirrorWebSite}" > data/src/main/resources/build.properties
