32e60c32df667ea11139618f6c2aacc8d1e70956
xiaochun <90xchun@gmail.com>
2025-04-13 11:31:12 +0800

    Try windows upload args

--- bin/upload-bin-windows.sh
+++ bin/upload-bin-windows.sh
@@ -2,4 +2,4 @@ bash -e bin/download-upload-depenecy.sh || true
 #upload
 originalPath="${4}/${5}"
 echo ${originalPath}
-pscp -unsafe -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
+pscp -hostkey "ssh-ed25519 255 SHA256:8r83U7DEUPSR49VK/E1VK9T+DGswIqO7wK6bjXHAD+s" -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
