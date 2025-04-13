6058cdc588ec4ffc9de37fd041458f29606ef24d
xiaochun <90xchun@gmail.com>
2025-04-13 12:02:27 +0800

    Try windows upload args

--- bin/upload-bin-windows.sh
+++ bin/upload-bin-windows.sh
@@ -2,5 +2,5 @@ bash -e bin/download-upload-depenecy.sh || true
 #upload
 originalPath="${4}/${5}"
 echo ${originalPath}
-echo y | plink -batch -pw ${2} ${1}@dl.zrlog.com echo "Hello"
+echo y | plink -pw ${2} ${1}@dl.zrlog.com "echo host trusted"
 pscp -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
