630cd1585ef0f2f91df635a72aa985a08cbcb0a1
xiaochun <90xchun@gmail.com>
2025-04-13 11:22:35 +0800

    Try unsafe upload args

--- bin/upload-bin-windows.sh
+++ bin/upload-bin-windows.sh
@@ -2,4 +2,4 @@ bash -e bin/download-upload-depenecy.sh || true
 #upload
 originalPath="${4}/${5}"
 echo ${originalPath}
-pscp -hostkey "ssh-ed25519 255 SHA256:FpsWLo+PNYccPlNgcfZKfOVAGIbawyEQ/xXzAmla1AY" -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
+pscp -unsafe -pw ${2} -r ${originalPath} ${1}@dl.zrlog.com:${3}
