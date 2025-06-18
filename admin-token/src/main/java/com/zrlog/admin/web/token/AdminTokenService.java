13e1ed4094a1b5404d1499154b9325c48d932d7c
xiaochun <90xchun@gmail.com>
2025-06-18 22:57:53 +0800

    Add war info

--- admin-token/src/main/java/com/zrlog/admin/web/token/AdminTokenService.java
+++ admin-token/src/main/java/com/zrlog/admin/web/token/AdminTokenService.java
@@ -8,7 +8,6 @@ import com.hibegin.common.util.StringUtils;
 import com.hibegin.http.server.api.HttpRequest;
 import com.hibegin.http.server.api.HttpResponse;
 import com.hibegin.http.server.web.cookie.Cookie;
-import com.zrlog.blog.web.util.WebTools;
 import com.zrlog.business.util.InstallUtils;
 import com.zrlog.common.Constants;
 import com.zrlog.common.TokenService;
@@ -117,6 +116,9 @@ public class AdminTokenService implements TokenService {
                 cookie.setValue("");
                 cookie.setExpireDate(new Date(0));
                 cookie.setPath("/");
+                if (Constants.isStaticHtmlStatus()) {
+                    cookie.setSameSite("None");
+                }
                 cookie.setHttpOnly(true);
                 response.addCookie(cookie);
             }
--- admin-web/src/main/java/com/zrlog/admin/web/interceptor/AdminCrossOriginInterceptor.java
+++ admin-web/src/main/java/com/zrlog/admin/web/interceptor/AdminCrossOriginInterceptor.java
@@ -12,17 +12,16 @@ public class AdminCrossOriginInterceptor extends AdminInterceptor {
 
     @Override
     public boolean doInterceptor(HttpRequest request, HttpResponse response) {
-        if (Objects.nonNull(ZrLogUtil.getBlogHostByWebSite())) {
+        String origin = request.getHeader("Origin");
+        if (Objects.nonNull(ZrLogUtil.getBlogHostByWebSite()) && Objects.nonNull(origin)) {
+            response.addHeader("Access-Control-Allow-Origin", origin);
+            response.addHeader("Access-Control-Allow-Credentials", "true");
             if (request.getMethod() == HttpMethod.OPTIONS) {
-                response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-                response.addHeader("Access-Control-Allow-Credentials", "true");
                 response.addHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
                 response.addHeader("Access-Control-Allow-Headers", "Content-Type");
                 response.write(new LengthByteArrayInputStream(new byte[0]), 200);
                 return false;
             }
-            response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-            response.addHeader("Access-Control-Allow-Credentials", "true");
         }
         return true;
     }
