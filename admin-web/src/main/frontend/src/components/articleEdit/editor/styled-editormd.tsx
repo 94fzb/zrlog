80b72fa01dc1cbf59edf6bb19fcc2644e337196c
xiaochun <90xchun@gmail.com>
2025-06-28 00:06:55 +0800

    Remove editormd

--- admin-web/src/main/frontend/src/components/articleEdit/editor/styled-editormd.tsx
+++ admin-web/src/main/frontend/src/components/articleEdit/editor/styled-editormd.tsx
@@ -9,6 +9,10 @@ export const StyledEditormd = styled("div")`
         box-shadow: none !important;
     }
 
+    .preview {
+        overflow: scroll;
+    }
+    
     .cm-scroller {
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'
     }
