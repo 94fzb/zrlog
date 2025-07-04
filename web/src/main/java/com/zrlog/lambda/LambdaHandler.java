dc650ef849f70efcbd705b84b475f3df6c80f209
xiaochun <90xchun@gmail.com>
2025-07-04 20:37:47 +0800

    support base64 response

--- web/src/main/java/com/zrlog/lambda/LambdaHandler.java
+++ web/src/main/java/com/zrlog/lambda/LambdaHandler.java
@@ -1,17 +1,10 @@
 package com.zrlog.lambda;
 
-import com.google.gson.Gson;
-import com.hibegin.common.util.IOUtil;
 import com.hibegin.http.server.ApplicationContext;
 import com.hibegin.http.server.api.HttpRequest;
 import com.hibegin.http.server.handler.HttpRequestHandlerRunnable;
-import com.hibegin.http.server.util.PathUtil;
 import com.zrlog.common.ZrLogConfig;
 import com.zrlog.lambda.rest.LambdaApiGatewayRequest;
-import jdk.jpackage.internal.IOUtils;
-
-import java.io.File;
-import java.util.logging.Logger;
 
 public class LambdaHandler {
 
