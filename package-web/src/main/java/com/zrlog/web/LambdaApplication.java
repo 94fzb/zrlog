c4b4eb87d285a7bbf57c7767d58868014e559891
xiaochun <90xchun@gmail.com>
2025-07-05 09:47:10 +0800

    Optimize query

--- package-web/src/main/java/com/zrlog/web/LambdaApplication.java
+++ package-web/src/main/java/com/zrlog/web/LambdaApplication.java
@@ -1,36 +1,18 @@
 package com.zrlog.web;
 
-import com.google.gson.Gson;
-import com.hibegin.common.util.EnvKit;
 import com.zrlog.business.service.NativeImageUpdater;
 import com.zrlog.common.Constants;
 import com.zrlog.common.type.RunMode;
+import com.zrlog.lambda.LambdaEventIterator;
 import com.zrlog.lambda.LambdaHandler;
 import com.zrlog.lambda.rest.LambdaApiGatewayRequest;
 import com.zrlog.lambda.rest.LambdaApiGatewayResponse;
 import com.zrlog.util.ZrLogUtil;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.AbstractMap;
 import java.util.Map;
-import java.util.logging.Logger;
 
 public class LambdaApplication {
-
-    private static final Logger LOGGER = Logger.getLogger(LambdaApplication.class.getName());
-
-    private static String getBaseUrl() {
-        return "http://" + System.getenv("AWS_LAMBDA_RUNTIME_API") + "/2018-06-01/runtime/invocation";
-    }
-
-    private static final HttpClient httpClient = HttpClient.newBuilder().build();
 
     static {
         initLambdaEnv();
@@ -47,51 +29,15 @@ public class LambdaApplication {
         System.getProperties().put("sws.root.path", System.getProperty("user.dir"));
     }
 
-    private static String getDevInput() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder inputBuilder = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            inputBuilder.append(line).append("\n");
-        }
-        return inputBuilder.toString().trim();
-    }
-
-    /**
-     * http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next
-     * @return
-     * @throws IOException
-     */
-    private static Map.Entry<String, LambdaApiGatewayRequest> getRequestInfo() throws IOException, InterruptedException {
-        if (EnvKit.isDevMode()) {
-            LambdaApiGatewayRequest apiGatewayRequest = new Gson().fromJson(getDevInput(), LambdaApiGatewayRequest.class);
-            return new AbstractMap.SimpleEntry<>(apiGatewayRequest.getRequestContext().getRequestId(), apiGatewayRequest);
-        }
-        HttpResponse<String> send = httpClient.send(HttpRequest.newBuilder(URI.create(getBaseUrl() + "/next")).build(), HttpResponse.BodyHandlers.ofString());
-        LambdaApiGatewayRequest apiGatewayRequest = new Gson().fromJson(send.body(), LambdaApiGatewayRequest.class);
-        LOGGER.info("lambda request = " + new Gson().toJson(apiGatewayRequest));
-        return new AbstractMap.SimpleEntry<>(send.headers().firstValue("Lambda-Runtime-Aws-Request-Id").orElse(""), apiGatewayRequest);
-    }
-
-    private static void report(String output, String requestId) throws IOException, InterruptedException {
-        HttpRequest.Builder builder = HttpRequest.newBuilder(URI.create(getBaseUrl() + "/" + requestId + "/response"));
-        builder.POST(HttpRequest.BodyPublishers.ofString(output));
-        HttpResponse<String> send = httpClient.send(builder.build(), HttpResponse.BodyHandlers.ofString());
-        LOGGER.info("lambda main server response = " + send.statusCode() + ":" + send.body());
-    }
-
     public static void doHandle(String[] args, int port, String execFile) throws Exception {
         Application.webServerBuilder(port, ZrLogUtil.getContextPath(args), new NativeImageUpdater(args, new File(execFile)));
+        LambdaEventIterator lambdaEventIterator = new LambdaEventIterator();
+        LambdaHandler lambdaHandler = new LambdaHandler(Constants.zrLogConfig);
         //处理请求
-        while (true) {
-            Map.Entry<String, LambdaApiGatewayRequest> requestInfo = getRequestInfo();
-            LambdaApiGatewayResponse apiGatewayResponse = new LambdaHandler(Constants.zrLogConfig).doHandle(getRequestInfo().getValue());
-            String output = new Gson().toJson(apiGatewayResponse);
-            LOGGER.info("lambda response = " + output);
-            if (EnvKit.isDevMode()) {
-                return;
-            }
-            report(output, requestInfo.getKey());
+        while (lambdaEventIterator.hasNext()) {
+            Map.Entry<String, LambdaApiGatewayRequest> requestInfo = lambdaEventIterator.next();
+            LambdaApiGatewayResponse apiGatewayResponse = lambdaHandler.doHandle(requestInfo.getValue());
+            lambdaEventIterator.report(apiGatewayResponse, requestInfo.getKey());
         }
     }
 }
---
+++ web/src/main/java/com/zrlog/lambda/LambdaEventIterator.java
@@ -0,0 +1,96 @@
+package com.zrlog.lambda;
+
+
+import com.google.gson.Gson;
+import com.hibegin.common.util.EnvKit;
+import com.zrlog.common.Constants;
+import com.zrlog.lambda.rest.LambdaApiGatewayRequest;
+import com.zrlog.lambda.rest.LambdaApiGatewayResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.AbstractMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Logger;
+
+/**
+ * <a href="https://docs.aws.amazon.com/lambda/latest/dg/runtimes-api.html">lambda runtime api</a>
+ */
+public class LambdaEventIterator implements Iterator<Map.Entry<String, LambdaApiGatewayRequest>> {
+
+    private static final Logger LOGGER = Logger.getLogger(LambdaEventIterator.class.getName());
+
+    private boolean hasNext = true;
+
+    private String getBaseUrl() {
+        return "http://" + System.getenv("AWS_LAMBDA_RUNTIME_API") + "/2018-06-01/runtime/invocation";
+    }
+
+    private final HttpClient httpClient = HttpClient.newBuilder().build();
+
+    private static String getDevInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder inputBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            inputBuilder.append(line).append("\n");
+        }
+        return inputBuilder.toString().trim();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return hasNext;
+    }
+
+    @Override
+    public Map.Entry<String, LambdaApiGatewayRequest> next() {
+        try {
+            Map.Entry<String, LambdaApiGatewayRequest> requestInfo = getRequestInfo();
+            if (Constants.debugLoggerPrintAble()) {
+                LOGGER.info("lambda request  " + requestInfo.getKey() + " : " + new Gson().toJson(requestInfo.getValue()));
+            }
+            return requestInfo;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next
+     * @return
+     * @throws IOException
+     */
+    private Map.Entry<String, LambdaApiGatewayRequest> getRequestInfo() throws IOException, InterruptedException {
+        if (EnvKit.isDevMode()) {
+            LambdaApiGatewayRequest apiGatewayRequest = new Gson().fromJson(getDevInput(), LambdaApiGatewayRequest.class);
+            return new AbstractMap.SimpleEntry<>(apiGatewayRequest.getRequestContext().getRequestId(), apiGatewayRequest);
+        }
+        HttpResponse<String> send = httpClient.send(HttpRequest.newBuilder(URI.create(getBaseUrl() + "/next")).build(), HttpResponse.BodyHandlers.ofString());
+        LambdaApiGatewayRequest apiGatewayRequest = new Gson().fromJson(send.body(), LambdaApiGatewayRequest.class);
+        return new AbstractMap.SimpleEntry<>(send.headers().firstValue("Lambda-Runtime-Aws-Request-Id").orElse(""), apiGatewayRequest);
+    }
+
+    public void report(LambdaApiGatewayResponse response, String requestId) throws IOException, InterruptedException {
+        String output = new Gson().toJson(response);
+        if (Constants.debugLoggerPrintAble()) {
+            LOGGER.info("lambda response " + requestId + " : " + output);
+        }
+        if (EnvKit.isDevMode()) {
+            hasNext = false;
+            return;
+        }
+        HttpRequest.Builder builder = HttpRequest.newBuilder(URI.create(getBaseUrl() + "/" + requestId + "/response"));
+        builder.POST(HttpRequest.BodyPublishers.ofString(output));
+        HttpResponse<String> send = httpClient.send(builder.build(), HttpResponse.BodyHandlers.ofString());
+        if (Constants.debugLoggerPrintAble()) {
+            LOGGER.info("lambda main server response = " + send.statusCode() + ":" + send.body());
+        }
+    }
+}
