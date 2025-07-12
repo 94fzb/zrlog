3d01c5cc24608c214a4bee0351c6cf5709f6ecc0
xiaochun <90xchun@gmail.com>
2025-07-12 19:55:08 +0800

    Add sync version info

--- bin/package-faas-zip.sh
+++ bin/package-faas-zip.sh
@@ -3,7 +3,8 @@ rm -rf conf/plugins/*
 mkdir -p conf/plugins/installed-plugins
 urls=(
   "static-plus-Linux-amd64.bin"
-#  "backup-sql-file-Linux-amd64.bin"
+  "rss-Linux-amd64.bin"
+  "changyan-file-Linux-amd64.bin"
   # 这里可以继续添加其他插件
 )
 UA="Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
