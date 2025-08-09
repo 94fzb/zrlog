a691fc3e6debed7c3e3ac65c3f5b4f4f1a5025fe
xiaochun <90xchun@gmail.com>
2025-08-09 23:08:43 +0800

    upgrade admin

--- doc/faas-deploy/cloudflare/pages-worker/index.js
+++ doc/faas-deploy/cloudflare/pages-worker/index.js
@@ -19,7 +19,7 @@ export default {
             });
         }
 
-        const lambdaUrl = env.LAMBDA_URL;
+        const lambdaUrl = env.BACKEND_SERVER_URL;
         const url = new URL(request.url);
         const newUrl = lambdaUrl + url.pathname + url.search;
 
