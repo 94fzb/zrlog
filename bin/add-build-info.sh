dfe33fb26895a7e8db485cb4d78049c61044c81a
xiaochun <90xchun@gmail.com>
2025-09-04 18:01:14 +0800

    Optimize build.properties

--- bin/add-build-info.sh
+++ bin/add-build-info.sh
@@ -5,11 +5,11 @@ version=$(printf 'VER\t${project.version}' | ./mvnw help:evaluate | grep '^VER' 
 mirrorWebSite=https://dl.zrlog.com/
 runMode=${1}
 if [[ $runMode == 'release' ]]; then
-runModeDesc="\u6B63\u5F0F\u7248\u672C"
+runModeDesc="\\u6B63\\u5F0F\\u7248\\u672C"
 elif [[ $runMode == 'preview' ]]; then
-runModeDesc="\u9884\u89C8\u7248\u672C"
+runModeDesc="\\u9884\\u89C8\\u7248\\u672C"
 else
-runModeDesc="\u5F00\u53D1\u7248\u672C"
+runModeDesc="\\u5F00\\u53D1\\u7248\\u672C"
 fi
 
 Date=$(git log -1 --format=%cd --date=format:'%Y-%m-%d %H:%M:%S%z' | sed "s/\([+-]\)\([0-9][0-9]\)\([0-9][0-9]\)/\1\2:\3/")
